92e4f7df0f918162e31f2fc392bcd3587504a4c1
Astrid Doncker <astrid.doncker@qonfucius.team>
2023-07-06 11:45:29 +0200

    fix : cleaned code

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ import favicon from "serve-favicon";
 import {
   S3Client,
   PutObjectCommand,
-  CreateBucketCommand,
   GetObjectCommand,
 } from "@aws-sdk/client-s3";
 import * as path from "path";
@@ -29,8 +28,6 @@ const storage = new Storage(
     : undefined
 );
 
-// Création bucket scaleway
-
 const client = new S3Client({
   endpoint: process.env.ENDPOINT,
   region: process.env.REGION,
@@ -40,49 +37,8 @@ const client = new S3Client({
   },
 });
 
-//Tests fonctionnels + get -> permet d'avoir un body lisible
-
-// PUT
-/*
-(async () => {
-  const response = await client.send(new PutObjectCommand({Bucket: process.env.SCALEWAY_BUCKET_NAME,Key:"tata.txt", Body: "toto"}));
-  console.log(response);
-})();*/
-
-//GET
-/*
-(async () => {
-  const client = new S3Client({
-    endpoint: process.env.ENDPOINT,
-    region: process.env.REGION,
-    credentials:{
-      accessKeyId: process.env.ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY as string
-    }
-  });
-
-  const streamToString = (stream: any) =>
-      new Promise((resolve, reject) => {
-        const chunks: any = [];
-        stream.on("data", (chunk: any) => chunks.push(chunk));
-        stream.on("error", reject);
-        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-      });
-
-  const command = new GetObjectCommand({
-    Bucket:  process.env.SCALEWAY_BUCKET_NAME,
-    Key: "toto.txt",
-  });
-
-  const { Body } = await client.send(command);
-  const bodyContents = await streamToString(Body);
-  console.log(bodyContents);
-})();
- */
-
 const bucket = storage.bucket(BUCKET_NAME);
 
-// Fonction permettant de savoir quel bucket est utilisé
 function getStorageType() {
   if (
     (process.env.SCALEWAY_BUCKET_NAME,
@@ -130,8 +86,6 @@ app.use(favicon(path.join(__dirname, "favicon.ico")));
 app.get("/", (req, res) => res.sendFile(`${process.cwd()}/index.html`));
 
 app.get("/api/v2/:key", corsGet, async (req: any, res: any) => {
-  //Début essai nouvelle condition GET
-
   if (getStorageType() === "S3") {
     try {
       await (async () => {
@@ -149,7 +103,7 @@ app.get("/api/v2/:key", corsGet, async (req: any, res: any) => {
 
         const command = new GetObjectCommand({
           Bucket: process.env.SCALEWAY_BUCKET_NAME,
-          Key: key, // Voir si ça marche pour key étant donné que j'ai juste repris le code d'en dessous
+          Key: key,
         });
 
         const { Body } = await client.send(command);
@@ -175,66 +129,10 @@ app.get("/api/v2/:key", corsGet, async (req: any, res: any) => {
   }
 });
 
-// Premier essai post mais le 2ème est peut-être plus "élégant" ?
-
-/*app.post("/api/v2/post/", corsPost, (req, res) => {
-  if (getStorageType() === "S3") {
-    try {
-    (async () => {
-      const response = await client.send(new PutObjectCommand({Bucket: process.env.SCALEWAY_BUCKET_NAME,Key:"tata.txt", Body: "toto"}));
-      console.log(response);
-    })();
-    }catch (error) {
-      console.error(error);
-      res.status(500).json({message: "Could not upload the data."});
-    }
-
-  } else {
-    try {
-      let fileSize = 0;
-      const id = nanoid();
-      const blob = bucket.file(id);
-      const blobStream = blob.createWriteStream({resumable: false});
-
-      blobStream.on("error", (error) => {
-        console.error(error);
-        res.status(500).json({message: error.message});
-      });
-
-      blobStream.on("finish", async () => {
-        res.status(200).json({
-          id,
-          data: `${LOCAL ? "http" : "https"}://${req.get("host")}/api/v2/${id}`,
-        });
-      });
-
-      req.on("data", (chunk) => {
-        blobStream.write(chunk);
-        fileSize += chunk.length;
-        if (fileSize > FILE_SIZE_LIMIT) {
-          const error = {
-            message: "Data is too large.",
-            max_limit: FILE_SIZE_LIMIT,
-          };
-          blobStream.destroy();
-          console.error(error);
-          return res.status(413).json(error);
-        }
-      });
-      req.on("end", () => {
-        blobStream.end();
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({message: "Could not upload the data."});
-    }
-  }
-});*/
-
 app.post("/api/v2/post/", corsPost, async (req, res) => {
   if (getStorageType() === "S3") {
     try {
-      await uploadToS3(req); // devoir transmettre req ou res
+      await uploadToS3(req);
       res.status(200).json({ message: "Data uploaded successfully." });
       console.log(res);
       console.log(req);
@@ -288,8 +186,8 @@ app.post("/api/v2/post/", corsPost, async (req, res) => {
     const bucketName = process.env.SCALEWAY_BUCKET_NAME;
     const command = new PutObjectCommand({
       Bucket: bucketName,
-      Key: req.params.key, // RÉUSSIR À CONSOLE LOG REQ
-      Body: req.params.body, // RÉUSSIR À CONSOLE LOG REQ
+      Key: req.params.key,
+      Body: req.params.body,
     });
 
     const response = await client.send(command);
